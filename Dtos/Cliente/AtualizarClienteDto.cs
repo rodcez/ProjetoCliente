e471961ad2beed9096cb5d2b0385fcadbdf41855
Rodrigo Cezario <contato.rodrigoc@gmail.com>
2020-08-02 21:12:02 -0300

    Ajsute no DTO de atualizar

--- Dtos/Cliente/AtualizarClienteDto.cs
+++ Dtos/Cliente/AtualizarClienteDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,15 @@ namespace Cliente_CRUD.Dtos.Cliente
 {
     public class AtualizarClienteDto
     {
+        [Required]
         public Guid Id { get; set; }
+
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Nome do Cliente: Deve ter entre 1 e 100 caracteres.")]
+        [Required(ErrorMessage = "Nome do Cliente: Campo Obrigatório.")]
         public string Nome { get; set; }
+
+        [Range(10, 100, ErrorMessage = "Idade do Cliente: Deve ser entre 10 e 100 anos.")]
+        [Required(ErrorMessage = "Idade do Cliente: Campo Obrigatório.")]
         public int Idade { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # ProjetoCliente
 
-Utilizar o profile: Cliente_CRUD
-
 Projeto em .Net Core 3.1 com: 
 - Swagger
 - Teste unitário
